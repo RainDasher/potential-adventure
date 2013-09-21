aa3e149f519a75921fe2e858ba9042089b0e3148
RainDasher <Raindasher@anypony.com>
2013-09-21 03:59:49 -0700

    moved shit
    and added start line

--- info.js
+++ info.js
@@ -1,4 +1,6 @@
+
 API.on(API.DJ_ADVANCE, songUpdate);
 function songUpdate(){
 API.sendChat(API.getDJs()[0].username + " is now playing " + API.getMedia().title + " by " + API.getMedia().author);
 }
+API.sendChat('Info Started');
